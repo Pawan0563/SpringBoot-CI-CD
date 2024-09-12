3e0e0d1ad6c1b8a9a555158fb135a46d329f8379
Pawan0563 <210101120084@cutm.ac.in>
2024-09-12 07:24:23 +0530

    first commit

---
+++ src/main/java/com/pawan/DevOpsCicdApplication.java
@@ -0,0 +1,13 @@
+package com.pawan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsCicdApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/pawan/controller/PawanController.java
@@ -0,0 +1,14 @@
+package com.pawan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PawanController {
+	@GetMapping
+	public String getMessage() {
+		return "Dabbang Chhora is Learning DevOps";
+	}
+	
+
+}
